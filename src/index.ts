59197dd5fa74ab8e198e6975de1ce608c2e3ab62
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-09-15 12:29:44 +0700

    chore: move server logic to index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
 import { env } from "./config";
-import app from "./server";
+
+import { Hono } from "hono";
+import { bot } from "./bot";
+
+const app = new Hono();
+
+// Health check
+app.get("/", (c) => {
+  return c.json({
+    success: true,
+    message: "🤖 Bot Telegram aktif!",
+  });
+});
+
+// Webhook endpoint
+app.post("/webhook", async (c) => {
+  try {
+    const update = await c.req.json(); // ambil body JSON dari request Telegram
+    await bot.handleUpdate(update); // lempar ke Telegraf
+    return c.text("ok");
+  } catch (err) {
+    console.error("Webhook error:", err);
+    return c.text("error", 500);
+  }
+});
 
 console.log(`🚀 Server berjalan di http://localhost:${env.PORT}`);
 
